02277808671248dfb37f5212eb64b1e8c741a2a5
James Best <jim@justjibba.net>
2023-12-18 05:33:19 +0000

    Base program

---
+++ main.lua
@@ -0,0 +1,14 @@
+-- three main functions: load, update, draw
+
+-- load is called once at the beginning of the game
+-- set game state, load assets, etc
+function love.load() end
+
+-- update is called every frame
+-- 60 frames a second
+function love.update(dt) end
+
+-- draw graphsics to the screen
+-- called every frame
+-- no calculations or logic
+function love.draw() end
