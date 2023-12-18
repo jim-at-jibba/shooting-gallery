47af5c1109ab82f252d919f4bb3f9e7dfdb9abcc
James Best <jim@justjibba.net>
2023-12-18 05:48:01 +0000

    Set up game state

--- main.lua
+++ main.lua
@@ -2,7 +2,15 @@
 
 -- load is called once at the beginning of the game
 -- set game state, load assets, etc
-function love.load() end
+function love.load()
+	target = {}
+	target.x = 300
+	target.y = 300
+	target.radius = 50
+
+	score = 0
+	timer = 0
+end
 
 -- update is called every frame
 -- 60 frames a second
@@ -17,6 +25,9 @@ function love.draw()
 
 	-- to use RGB colors with 255 scale /255
 	-- love.graphics.setColor(0/255, 0/255, 255/255)
-	love.graphics.setColor(0, 0, 1)
-	love.graphics.circle("fill", 100, 100, 50)
+	-- love.graphics.setColor(0, 0, 1)
+	-- love.graphics.circle("fill", 100, 100, 50)
+
+	love.graphics.setColor(1, 0, 0)
+	love.graphics.circle("fill", target.x, target.y, target.radius)
 end
